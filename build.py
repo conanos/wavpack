2b1c91a56e99407e34461cfdb806f3c4a59154f3
cppbitman <daihongjun@kedacom.com>
2018-12-17 15:12:59 +0800

    windows build done

---
+++ build.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+from conanos.build import Main
+
+if __name__ == "__main__":
+    Main('wavpack',pure_c=True)
--- conanfile.py
+++ conanfile.py
@@ -1,43 +1,77 @@
-from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
-import os
+from conans import ConanFile, tools, AutoToolsBuildEnvironment, MSBuild
+from conanos.build import config_scheme
+import os, shutil
 
 class WavpackConan(ConanFile):
     name = "wavpack"
     version = "5.1.0"
     description = "WavPack is a completely open audio compression format providing lossless, high-quality lossy, and a unique hybrid compression mode"
     url = "https://github.com/conanos/wavpack"
-    license = "BSD_like"
+    homepage = "http://www.wavpack.com/"
+    license = "BSD"
+    patch = "msvc-afxres-h-not-existing.patch"
+    exports = ["COPYING", patch]
+    generators = "visual_studio", "gcc"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
-    generators = "cmake"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = { 'shared': True, 'fPIC': True }
 
-    source_subfolder = "source_subfolder"
-    
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+        config_scheme(self)
+
     def source(self):
-        url_ = 'https://github.com/dbry/WavPack/archive/%s.tar.gz'%(self.version)
-        tools.get(url_)
+        url_ = 'https://github.com/dbry/WavPack/archive/{version}.tar.gz'
+        tools.get(url_.format(version=self.version))
         extracted_dir = "WavPack-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        if self.settings.os == 'Windows':
+            tools.patch(patch_file=self.patch)
+        os.rename(extracted_dir, self._source_subfolder)
 
     def build(self):
-        with tools.chdir(self.source_subfolder):
-            self.run("autoreconf -f -i")
+        #with tools.chdir(self.source_subfolder):
+        #    self.run("autoreconf -f -i")
 
-            autotools = AutoToolsBuildEnvironment(self)
-            _args = ["--prefix=%s/builddir"%(os.getcwd()), "--disable-apps"]
-            if self.options.shared:
-                _args.extend(['--enable-shared=yes','--enable-static=no'])
-            else:
-                _args.extend(['--enable-shared=no','--enable-static=yes'])
-            autotools.configure(args=_args)
-            autotools.make(args=["-j4"])
-            autotools.install()
+        #    autotools = AutoToolsBuildEnvironment(self)
+        #    _args = ["--prefix=%s/builddir"%(os.getcwd()), "--disable-apps"]
+        #    if self.options.shared:
+        #        _args.extend(['--enable-shared=yes','--enable-static=no'])
+        #    else:
+        #        _args.extend(['--enable-shared=no','--enable-static=yes'])
+        #    autotools.configure(args=_args)
+        #    autotools.make(args=["-j4"])
+        #    autotools.install()
+
+        if self.settings.os == 'Windows':
+            with tools.chdir(os.path.join(self._source_subfolder)):
+                msbuild = MSBuild(self)
+                msbuild.build("wavpack.sln",upgrade_project=True,platforms={'x86': 'Win32', 'x86_64': 'x64'})
+
 
     def package(self):
-        if tools.os_info.is_linux:
-            with tools.chdir(self.source_subfolder):
-                self.copy("*", src="%s/builddir"%(os.getcwd()))
+        if self.settings.os == 'Windows':
+            platforms={'x86': 'Win32', 'x86_64': 'x64'}
+            output_rpath = os.path.join(platforms.get(str(self.settings.arch)), str(self.settings.build_type))
+            if self.options.shared:   
+                self.copy("wavpackdll.*", dst=os.path.join(self.package_folder,"lib"),
+                          src=os.path.join(self.build_folder,self._source_subfolder,output_rpath), excludes="wavpackdll.dll")
+                self.copy("wavpackdll.dll", dst=os.path.join(self.package_folder,"bin"),
+                          src=os.path.join(self.build_folder,self._source_subfolder,output_rpath))
+            else:
+                self.copy("libwavpack.*", dst=os.path.join(self.package_folder,"lib"),
+                          src=os.path.join(self.build_folder,self._source_subfolder,output_rpath))
+
+        #if tools.os_info.is_linux:
+        #    with tools.chdir(self.source_subfolder):
+        #        self.copy("*", src="%s/builddir"%(os.getcwd()))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
